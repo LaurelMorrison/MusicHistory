dce5b6fe428e97d97f77eeb9dfedae7721780063
Laurel <lbentleym1992@gmail.com>
2021-06-02 12:47:06 -0400

    adding file structure

---
+++ MusicHistory.sql
@@ -0,0 +1,17 @@
+﻿/*
+SELECT
+    Id,
+    Title,
+    SongLength,
+    ReleaseDate,
+    GenreId,
+    ArtistId,
+    AlbumId
+FROM Song
+WHERE SongLength > 100;
+*/
+
+SELECT s.Title,
+       a.ArtistName
+  FROM Song s
+       LEFT JOIN Artist a on s.ArtistId = a.id;
